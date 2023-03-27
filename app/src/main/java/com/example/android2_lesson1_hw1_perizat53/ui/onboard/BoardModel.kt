a5a98d327aefc414eed334536e3065d27bea4e24
Perizat30 <perizatk39@gmail.com>
2023-03-28 02:49:24 +0600

    I just took in comment StartActivityForResult ,since it was deprecated,instead wrote another one. Rewrote all codes from 2nd lesson. Put dotsIndicators,  realized buttons.

---
+++ app/src/main/java/com/example/android2_lesson1_hw1_perizat53/ui/onboard/BoardModel.kt
@@ -0,0 +1,10 @@
+package com.example.android2_lesson1_hw1_perizat53.ui.onboard
+
+data class BoardModel(
+    var image:Int,
+    var title:String,
+    var description:String,
+    var isLast:Boolean
+
+):java.io.Serializable
+
